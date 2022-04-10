3f4f7cf514d37ab34d417b8e2932dc2dde74a40a
Brenton Babb babbb@wit.edu <thebubfusion@gmail.com>
2022-04-10 19:32:38 -0400

    initial commit

---
+++ Mastermind/src/Main.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	public static void main(String[] args) {
+		launch(args);
+	}
+	public void start(Stage primaryStage) throws Exception {
+
+		Pane p = new Pane();
+		Scene s = new Scene(p, 400, 400);
+		primaryStage.setScene(s);
+		primaryStage.show();
+	}
+}
