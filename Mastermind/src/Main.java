7f308a318a65b2dd75bb60d5a1329c5eb197eb8b
Brenton Babb babbb@wit.edu <thebubfusion@gmail.com>
2022-04-14 18:58:45 -0400

    FMXL file and JavaFX stuff

--- Mastermind/src/Main.java
+++ Mastermind/src/Main.java
@@ -1,19 +1,37 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application implements Initializable{
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	@FXML
+	Button b;
+	
 	public void start(Stage primaryStage) throws Exception {
 
-		Pane p = new Pane();
-		Scene s = new Scene(p, 400, 400);
-		primaryStage.setScene(s);
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Mastermind.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Mastermind");
+		primaryStage.setScene(scene);
 		primaryStage.show();
 		Tile masterTile = GameMaster.answerGen();
 		System.out.println(masterTile.toString());
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resourses) {
+		
 	}
 }
